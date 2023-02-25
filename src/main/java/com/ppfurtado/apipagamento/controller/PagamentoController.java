367646ce5f235354dd06f1280c6be08bef7c9b3e
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-02-25 00:38:14 -0300

    feat: método para buscar todos os pagamentos e salvar pagamento

--- src/main/java/com/ppfurtado/apipagamento/controller/PagamentoController.java
+++ src/main/java/com/ppfurtado/apipagamento/controller/PagamentoController.java
@@ -1,6 +1,34 @@
 package com.ppfurtado.apipagamento.controller;
 
+
+import com.ppfurtado.apipagamento.domain.dto.PagamentoRequest;
+import com.ppfurtado.apipagamento.domain.dto.PagamentoResponse;
+import com.ppfurtado.apipagamento.domain.model.Pagamento;
+import com.ppfurtado.apipagamento.domain.service.PagamentoService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/api/pagamento")
 public class PagamentoController {
 
+    private final PagamentoService service;
+
+
+    public PagamentoController(PagamentoService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/salvar")
+    public PagamentoResponse save(@RequestBody @Validated PagamentoRequest request){
+        return service.save(request);
+    }
+
+    @GetMapping("/buscar-pagamentos")
+    public List<Pagamento> findAll(){
+        return service.findAll();
+    }
 
 }
