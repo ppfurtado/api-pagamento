8618c5dee7b8b293f32ac4a750833cc82eb43262
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-02-25 00:35:44 -0300

    refactor: alterando o retorno do método save

--- src/main/java/com/ppfurtado/apipagamento/domain/service/PagamentoService.java
+++ src/main/java/com/ppfurtado/apipagamento/domain/service/PagamentoService.java
@@ -1,23 +1,26 @@
 package com.ppfurtado.apipagamento.domain.service;
 
 import com.ppfurtado.apipagamento.domain.dto.PagamentoRequest;
-import com.ppfurtado.apipagamento.domain.dto.mapper.PagamentoMapper;
+import com.ppfurtado.apipagamento.domain.dto.PagamentoResponse;
+import com.ppfurtado.apipagamento.domain.mapper.PagamentoMapper;
 import com.ppfurtado.apipagamento.domain.model.Pagamento;
 import com.ppfurtado.apipagamento.domain.repository.PagamentoRepository;
+import org.springframework.stereotype.Service;
 
 public class PagamentoService {
 
-    private final PagamentoRepository repository;
-    private final PagamentoMapper mapper;
 
-    public PagamentoService(PagamentoRepository pagamentoRepository, PagamentoMapper mapper) {
-        this.repository = pagamentoRepository;
+    private final PagamentoMapper mapper;
+    private final PagamentoRepository repository;
+
+    public PagamentoService(PagamentoMapper mapper, PagamentoRepository pagamentoRepository) {
         this.mapper = mapper;
+        this.repository = pagamentoRepository;
     }
 
-    public Pagamento save(PagamentoRequest request){
+    public PagamentoResponse save(PagamentoRequest request){
         Pagamento pagamento = mapper.requestToPagamento(request);
-        return repository.save(pagamento);
+        return mapper.pagamentoToResponse(repository.save(pagamento));
     }
 
 
