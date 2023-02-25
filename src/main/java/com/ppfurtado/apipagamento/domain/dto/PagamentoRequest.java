744ab3117b348554d745485c69d7f37e13289786
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-02-25 00:33:58 -0300

    refactor: adicionando validações para os campos

--- src/main/java/com/ppfurtado/apipagamento/domain/dto/PagamentoRequest.java
+++ src/main/java/com/ppfurtado/apipagamento/domain/dto/PagamentoRequest.java
@@ -1,15 +1,25 @@
 package com.ppfurtado.apipagamento.domain.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Max;
 
 public record PagamentoRequest(
-        @NotBlank
+        @NotNull
         int codigoPagamento,
+
         @NotBlank
+        @Max(14)
+        @Min(11)
         String cpfCnpj,
+
         @NotBlank
+        @Max(19)
+        @Min(13)
         String numeroCartao,
-        @NotBlank
+
+        @NotNull
         double valorPagamento) {
 
 }
