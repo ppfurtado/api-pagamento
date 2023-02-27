63b8aa122b3610f7a9fa9fc787dad0bdb7892965
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-02-27 18:41:27 -0300

    refactr: removendo codigoPagamento, adicionando metodoPagamento e validações

--- src/main/java/com/ppfurtado/apipagamento/domain/dto/PagamentoRequest.java
+++ src/main/java/com/ppfurtado/apipagamento/domain/dto/PagamentoRequest.java
@@ -1,22 +1,16 @@
 package com.ppfurtado.apipagamento.domain.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.*;
 
 public record PagamentoRequest(
-        @NotNull
-        int codigoPagamento,
 
         @NotBlank
-        @Max(14)
-        @Min(11)
+        @Size(min = 9, max = 14, message = "O campo cpf_cnpj é entre 11 and 14")
         String cpfCnpj,
 
         @NotBlank
-        @Max(19)
-        @Min(13)
+        String metodoPagamento,
+
         String numeroCartao,
 
         @NotNull
--- src/main/java/com/ppfurtado/apipagamento/domain/dto/PagamentoResponse.java
+++ src/main/java/com/ppfurtado/apipagamento/domain/dto/PagamentoResponse.java
@@ -1,10 +1,15 @@
 package com.ppfurtado.apipagamento.domain.dto;
 
+
+import com.ppfurtado.apipagamento.domain.enums.MetodoPagamentoEnum;
+import com.ppfurtado.apipagamento.domain.enums.StatusEnum;
+
 public record PagamentoResponse(
         Long id,
-        int codigoPagamento,
         String cpfCnpj,
+        MetodoPagamentoEnum metodoPagamento,
         String numeroCartao,
-        double valorPagamento
+        double valorPagamento,
+        StatusEnum status
 ) {
 }
